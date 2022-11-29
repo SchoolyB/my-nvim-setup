d90dba2231f99f6bfbb1d1a76ce08334759405cb
Schooly <marshallbcodes@gmail.com>
2022-11-29 16:27:57 -0600

    Initial Commit

---
+++ init.lua
@@ -0,0 +1,7 @@
+require("schooly.plugins-setup")
+require("schooly.core.options")
+require("schooly.core.keymaps")
+require("schooly.core.colorscheme")
+
+
+
---
+++ lua/schooly/core/options.lua
@@ -0,0 +1,49 @@
+local opt = vim.opt
+
+--setting up line numbers
+
+opt.relativenumber = true
+opt.number = true
+
+
+--tabbing and indentation
+
+opt.tabstop=2
+opt.shiftwidth=2
+opt.expandtab = true
+opt.autoindent = true
+
+
+--line wrapping
+
+opt.wrap = false
+
+
+--search settings
+opt.ignorecase = true
+opt.smartcase = true
+
+--cursor line 
+opt.cursorline = true
+
+--appearcance
+opt.termguicolors = true
+opt.background = "dark"
+opt.signcolumn = "yes"
+
+
+--backspace
+opt.backspace = "indent,eol,start"
+
+--clipboard
+opt.clipboard:append("unnamedplus")
+
+--splitting windows
+opt.splitright = true
+opt.splitbelow = true
+
+
+--mouse functionallity
+opt.mouse = "a"
+
+opt.iskeyword:append("-")
